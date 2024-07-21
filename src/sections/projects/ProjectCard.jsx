4fde635e16adef398312c9cbab4db252f1017e48
akshayxemo <akshaykrdas001@gmail.com>
2024-07-22 01:32:28 +0530

    card view fix

--- src/sections/projects/ProjectCard.jsx
+++ src/sections/projects/ProjectCard.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import { Card, CardContent, CardFooter } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 const ProjectCard = () => {
   return (
     <>
-    <Card className='w-[295px] sm:w-[350px] h-fit p-0 overflow-hidden'>
-              <CardContent className='p-0'>
-                <div className='flex flex-col gap-3 items-center'>
-                  <img
-                    className='max-h-[20rem] w-full'
-                    src='https://images.unsplash.com/photo-1719937050814-72892488f741?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D'
-                    alt=''
-                  />
-                  <p className='text-justify px-4 pb-4'>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Repudiandae nostrum distinctio sint nihil officiis ipsa
-                    ullam recusandae maiores.
-                  </p>
-                </div>
-              </CardContent>
-              <CardFooter className='flex justify-center w-full '>
-                <Button size='lg'>Deploy</Button>
-              </CardFooter>
-            </Card>
+      <Card className="h-fit p-0 overflow-hidden">
+        <CardContent className="p-0">
+          <div className="flex flex-col gap-3 items-center">
+            <img
+              className="max-h-[20rem] w-full"
+              src="https://images.unsplash.com/photo-1719937050814-72892488f741?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+            <p className="text-justify px-4 pb-4">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repudiandae nostrum distinctio sint nihil officiis ipsa ullam
+              recusandae maiores.
+            </p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-center w-full ">
+          <Button size="lg">Deploy</Button>
+        </CardFooter>
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/sections/projects/index.jsx
+++ src/sections/projects/index.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
       <main className="w-full h-fit py-6 px-10 max-w-[90%] mx-auto flex flex-col gap-4 items-center">
         <hr className="w-full" />
         <h1 className="font-bold text-center text-4xl">Projects</h1>
-        <div className="flex items-center w-full gap-8">
+        <div className="flex items-center w-full gap-2 mt-4">
           <Button size="lg" variant="outline" className="flex-1">
             <p className="text-xl font-semibold">NLP</p>
           </Button>
@@ -20,7 +20,12 @@ const Projects = () => {
           <ProjectsGrid />
           <MobileCarousel />
           <div className="w-full text-center">
-            <Button size="lg">Load More</Button>
+            <Button
+              size="lg"
+              className="bg-transparent text-blue-500 text-lg hover:bg-transparent hover:underline"
+            >
+              Load More
+            </Button>
           </div>
         </section>
       </main>
