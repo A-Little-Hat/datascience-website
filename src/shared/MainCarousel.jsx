193d70a00b189b9705fa187386eb632d96abf15a
Pritam Sarkar <sarkarpritam0007@gmail.com>
2024-07-21 21:57:50 +0530

    Some changes

--- src/shared/MainCarousel.jsx
+++ src/shared/MainCarousel.jsx
@@ -5,10 +5,10 @@ const MainCarousel = () => {
   return (
     <>
     <div className="h-[12rem] md:h-[30rem]">
-      <Carousel>
-        <img src="https://www.caluniv.ac.in/images/slider/slider-00.jpg" alt="..." />
-        <img src="https://www.caluniv.ac.in/images/slider/slider-01.jpg" alt="..." />
-        <img src="https://www.caluniv.ac.in/images/slider/slider-02.jpg" alt="..." />
+      <Carousel className='rounded-none'>
+        <img className='h-full rounded-none' src="https://www.caluniv.ac.in/images/slider/slider-00.jpg" alt="..." />
+        <img className='h-full rounded-none' src="https://www.caluniv.ac.in/images/slider/slider-01.jpg" alt="..." />
+        <img className='h-full rounded-none' src="https://www.caluniv.ac.in/images/slider/slider-02.jpg" alt="..." />
       </Carousel>
     </div>
     </>
