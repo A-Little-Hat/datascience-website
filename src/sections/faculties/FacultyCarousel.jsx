56d76a0abc7aacb9dfe35008fae864dc4888283a
Pritam Sarkar <sarkarpritam0007@gmail.com>
2024-07-21 22:00:38 +0530

    Some changes

--- src/sections/faculties/FacultyCarousel.jsx
+++ src/sections/faculties/FacultyCarousel.jsx
@@ -20,7 +20,7 @@ const FacultyCarousel = () => {
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index} className='pl-1 md:basis-1/2 lg:basis-1/4'>
             <div className='p-1 grid place-items-center -ml-3 sm:-ml-0'>
-              <Card className='w-[350px] h-fit pt-5 border-none shadow-none'>
+              <Card className='w-[300px] sm:w-[350px] h-fit pt-5 border-none shadow-none'>
                 <CardContent>
                   <div className='flex flex-col gap-3 items-center'>
                     <img className='w-36 h-36 rounded-[50%] object-cover object-center' src='https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfDF8MHx8fDA%3D' alt='' />
