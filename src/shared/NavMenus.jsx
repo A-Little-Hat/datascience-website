0f02a82f6a59b2c0c78fa43dbc6df83202725288
akshayxemo <akshaykrdas001@gmail.com>
2024-07-22 00:36:59 +0530

    navbar ui adjustment

--- src/shared/NavMenus.jsx
+++ src/shared/NavMenus.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-
 const NavMenus = () => {
   return (
     <>
-      <ul className='flex flex-col md:flex-row p-2 items-start gap-4 w-full'>
-        <li className={`whitespace-nowrap flex items-center rounded-md font-medium transition-colors hover:bg-accent hover:text-accent-foreground px-4 py-3 w-full ${true && 'text-accent-foreground bg-accent'} ${(true) && 'mt-auto'}`}>
-          <a href='#' className={`inline-flex gap-3 text-sm sm:text-[1rem] items-center justify-start`}>Data Science</a>
+      <ul className="flex flex-col md:flex-row p-2 items-start gap-3 w-full">
+        <li
+          className={`whitespace-nowrap flex items-center rounded-md font-medium transition-colors hover:bg-accent hover:text-accent-foreground px-4 py-3 w-full ${
+            true && "text-accent-foreground bg-accent"
+          } ${true && "mt-auto"}`}
+        >
+          <a
+            href="#"
+            className={`inline-flex gap-3 text-sm sm:text-[1rem] items-center justify-start`}
+          >
+            Data Science
+          </a>
         </li>
-        <li className={`whitespace-nowrap flex items-center rounded-md font-medium transition-colors hover:bg-accent hover:text-accent-foreground px-4 py-3 w-full ${true && 'text-accent-foreground bg-accent'} ${(true) && 'mt-auto'}`}>
-          <a href='#' className={`inline-flex gap-3 text-sm sm:text-[1rem] items-center justify-start`}>IOT</a>
+        <li
+          className={`whitespace-nowrap flex items-center rounded-md font-medium transition-colors hover:bg-accent hover:text-accent-foreground px-4 py-3 w-full ${
+            true && "text-accent-foreground bg-accent"
+          } ${true && "mt-auto"}`}
+        >
+          <a
+            href="#"
+            className={`inline-flex gap-3 text-sm sm:text-[1rem] items-center justify-start`}
+          >
+            IOT
+          </a>
         </li>
       </ul>
     </>
