559b6307246710ac096b8278d8715312b74070f6
akshayxemo <akshaykrdas001@gmail.com>
2024-07-22 01:21:42 +0530

    footer and layout fix

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-
 const Footer = () => {
   return (
     <>
-    <div className='h-[5vh] flex items-center justify-center bg-slate-950 text-white'>
-        <p className='text-sm sm:text-base'>Copyright &#xa9; {new Date().getFullYear()} | AKCSIT, University of Calcutta</p>
-    </div>
+      <div className="h-min-[5vh] flex items-center justify-center bg-slate-950 text-white">
+        <p className="text-sm sm:text-base py-6">
+          Copyright &#xa9; {new Date().getFullYear()} | AKCSIT, University of
+          Calcutta
+        </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
